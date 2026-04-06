f267117f5902ec0ee4a0f0b19112ac8d16c5723e
phanatcha <phanatcha19@gmail.com>
2026-04-06 18:05:49 +0700

    Setup project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './index.css'
+
+function App() {
+
+  return (
+    <div className="h-screen flex items-center justify-center bg-black">
+      <h1 className="text-4xl text-white font-bold">
+        SCENTSTORY IS COMING SOON
+      </h1>
+    </div>
+  )
+}
+
+export default App
